d1ae1e125d44336992ce5d50b43a3e85908c061c
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-08-15 12:13:54 +0200

    Try and find the tables primary key

--- Model.php
+++ Model.php
@@ -27,18 +27,32 @@ class Model extends \CI_Model
      */
     public $primaryKey = "id";
 
-    public function __construct()
+    public function __construct($table = "")
     {
         parent::__construct();
 
         $this->load->helper("inflector");
+        $this->table = $table;
         $this->_setTable();
+        $this->_setPrimaryKey();
     }
 
+    /**
+     * Auto-guess the table name
+     */
     protected function _setTable()
     {
         if ($this->table === "") {
             $this->table = plural(preg_replace("/(_m|_model)?$/", "", strtolower(get_class($this))));
         }
+    }
+    
+    /**
+     * Try and retrieve the primary key from the table
+     */
+    protected function _setPrimaryKey()
+    {
+        $this->primaryKey = $this->db->query("SHOW KEYS FROM `{$this->table}` WHERE key_name = 'PRIMARY'")
+            ->row()->Column_name;
     }
 }
