2d35013d224481d0517aa3016e6c4b677d27a1d9
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-09-11 14:28:11 +0200

    Add primary key constants

--- Constants.php
+++ Constants.php
@@ -14,4 +14,11 @@ class Constants
     const DELETEHARD = 100;
     const DELETESOFTMARK = 101;
     const DELETESOFTSTATUS = 102;
+
+    /**
+     * Primary key Constants
+     */
+    const PKEYINT = 200;
+    const PKEYAI = 201;
+    const PKEYFUNC = 202;
 }
