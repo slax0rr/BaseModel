37b5daa25f66d1079d648b873023413187fa12ab
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-09-23 11:56:41 +0200

    Construct the string where statement from the objects

--- Where/Builder.php
+++ Where/Builder.php
@@ -11,10 +11,12 @@ use Expression as E;
 class Builder
 {
     protected $_expressions = array();
+    protected $_currentIndex = 0;
 
     public function add(
         $column,
         $value,
+        $table = "",
         $logicalOperator = "=",
         $comparator = "AND",
         $groupBegin = false,
@@ -26,16 +28,38 @@ class Builder
 
         $expr = new E();
         $expr->column = $column;
-        $expr->value = $this->_prepareValue($value);
+        $expr->value = $value;
+        $expr->table = $table;
         $expr->placeholder = is_string($value);
         $expr->comparator = $comparator;
         $expr->groupBegin = $groupBegin;
         $expr->groupEnd = $groupEnd;
         $this->_expressions[] = $expr;
+
+        return $this;
     }
 
-    protected function _prepareValue($value)
+    public function get()
     {
-        return $value;
+        if (isset($this->_expressions[$this->_currentIndex])) {
+            return $this->_expressions[$this->_currentIndex++];
+        } else {
+            return null;
+        }
+    }
+
+    public function reset()
+    {
+        $this->_currentIndex = 0;
+        return $this;
+    }
+
+    public function toString()
+    {
+        $where = "";
+        foreach ($this->_expressions as $e) {
+            $where .= (string)$e;
+        }
+        return $where;
     }
 }
--- Where/Expression.php
+++ Where/Expression.php
@@ -10,11 +10,68 @@ class Expression
 {
     public $column = "";
     public $value = null;
+    public $table = "";
     public $placeholder = false;
-    public $logicalOperator = "";
     public $comparator = "";
     public $groupBegin = false;
     public $groupEnd = false;
-    public $prev = null;
-    public $next = null;
+
+    public $binds = array();
+
+    public function __toString()
+    {
+        $value = $this->_prepareValue($this->value);
+
+        $expr = "";
+        // add the logical operator
+        $expr .= "{$this->logicalOperator} ";
+
+        // check if we need to go into a subgroup
+        if ($this->groupBegin) {
+            $expr .= "(";
+        }
+
+        // prepare column
+        if ($this->table !== "") {
+            $expr .= "`{$this->table}`.";
+        }
+        $expr .= "`{$this->column}`";
+
+        // add a comparator
+        $expr .= " {$this->comparator} ";
+
+        // prepare and add the value
+        $expr .= $this->_prepareValue($this->value);
+
+        // check if we need to end the subgroup
+        if ($this->groupEnd) {
+            $expr .= ")";
+        }
+
+        return $expr;
+    }
+
+    protected function _prepareValue($value)
+    {
+        $preparedValue = $value;
+        // if its a list construct
+        if (is_array($value)) {
+            $preparedValue = "(";
+            foreach ($value as $v) {
+                $preparedValue .= "?,";
+            }
+            $preparedValue = rtrim($preparedValue, ",") . ")";
+            $this->binds = array_merge($this->binds, $value);
+        }
+
+        if (is_string($value)) {
+            $preparedValue = "?";
+            $this->binds[] = $value;
+        }
+
+        if (is_object($value)) {
+            $preparedValue = (string)$value;
+        }
+        return $preparedValue;
+    }
 }
