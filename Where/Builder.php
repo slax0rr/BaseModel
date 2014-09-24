dcb8667ff4edcc7b58e341b9264fdecb83f19fe2
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-09-24 23:16:11 +0200

    Convert expression bool to int

--- Where/Builder.php
+++ Where/Builder.php
@@ -30,6 +30,9 @@ class Builder
         if ($comparator === "") {
             $comparator = "=";
         }
+        if (is_bool($value)) {
+            $value = ($value === true) ? 1 : 0;
+        }
 
         $expr = new E();
         $expr->column = $column;
